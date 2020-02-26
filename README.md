3a5001eaf81a4667c38acc4e79ab5201f70f0810
Anthony Riley II <riley@anth1y.io>
2020-02-25 21:37:09 -0800

    Add clap and some help flags

---
+++ README.md
@@ -0,0 +1,10 @@
+# sktsnk
+
+pronounced SocketSneak
+
+## About
+
+* A simple ass mock [tcpdump](https://www.tcpdump.org/) written in rust
+
+* We're of course utilizing [pcap](https://github.com/ebfull/pcap) & [clap](https://github.com/clap-rs/clap)
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 extern crate pcap;
+extern crate clap;
+use clap::App;
 
 fn main() {
+    App::new("sktsnk")
+        .version("0.1.0")
+        .about("Does tcpdump like things")
+        .author("Anthony Riley")
+        .usage("sktsnk [port src dest dev out] <file.pcap>")
+        .get_matches();
     let mut cap = pcap::Device::lookup().unwrap().open().unwrap();
     println!("{:?}", cap.next());
 }
